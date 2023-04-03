73ca70e8b366ab2cbb77e26fff1e021a00cdc2c7
surajkadm <68494225+surajkadm@users.noreply.github.com>
2023-04-04 01:16:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ function palindrome(str){
 	strLen=str.length;
 	var a=null;
 	for (let index = 0; index < strLen/2; index++) {
-		if(str[i]!===str[strLen-1-i]){
+		if(str[i]!==str[strLen-1-i]){
 				a=false;
 		}
-		else{
-			a=true;
-		}
+		a=true;
 		
 	}
-	console.log(a);
 	return a;
 }
 module.exports = palindrome
